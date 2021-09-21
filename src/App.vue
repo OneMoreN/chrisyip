c95cb78495af31b7fa4ca597993ebb1882f5c2c6
Chris Yip <christopheryipeio@gmail.com>
2021-09-21 18:37:13 +1000

    Sidebar changes

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,34 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <div id="topnav">
+    <img class="logo" alt="CY" src="@/assets/logo.png">
+  </div>
+  <div class="sidebar">
+    <SidebarLink
+      :route="'/'"
+      :navText="'Home'"
+      :faIcon="'fas fa-home'"
+      :hoverFaIcon="'fas fa-home'"
+    />
+    <SidebarLink
+      :route="'/about'"
+      :navText="'About'"
+      :faIcon="'far fa-circle'"
+      :hoverFaIcon="'fas fa-circle'"
+    />
   </div>
   <router-view/>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+import SidebarLink from '@/components/SidebarLink.vue'
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
+export default {
+  components: {
+    SidebarLink
+  },
+  data () {
+    return {
     }
   }
 }
-</style>
+</script>
---
+++ src/components/Landing.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+    Hello! <br/>
+    My name is Christopher Yip <br/>
+    I'm a Software Engineer
+  </div>
+</template>
